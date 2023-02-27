522745d146a173e55d3f87b6118f654cbdbb5a15
DragonT13 <alex23456@naver.com>
2023-02-27 23:10:56 +0900

    Minesweeper wip

--- src/Minesweeper/MainClass.cs
+++ src/Minesweeper/MainClass.cs
@@ -6,8 +6,45 @@ namespace Minesweeper
 	{
 		public static void Main(string[] args)
 		{
-            Console.Write("선택할 칸을 입력하시오(ex: 1 2):");
-			Console.ReadLine();
-        }
+			int[,] board = new int[9, 9];
+			int count = 1;
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					board[i, j] = count;
+					count++;
+				}
+			}
+
+			Minesweeper.MakeMine(board);
+			Minesweeper.ShowBoard(board);
+			Console.WriteLine();
+
+			Console.Write("선택할 칸을 입력하시오(1 1 ~ 9 9): ");
+			string input = Console.ReadLine();
+			string[] str = input.Split(" ");
+			int[] integers = new int[2];
+			for (int i = 0; i < integers.Length; i++)
+			{
+				integers[i] = int.Parse(str[i]);
+			}
+			int x = integers[0] - 1;
+			int y = integers[1] - 1;
+
+			if (board[x, y] == -1)
+			{
+				Console.WriteLine("지뢰를 밟았습니다!");
+				Console.WriteLine("-----Game Over-----");
+			}
+			else
+			{
+				// 칸 선택시 그 칸을 중심으로 한 3x3 영역에 몇 개의 지뢰가 존재하는지 표시
+				// 칸 주변에 지뢰가 하나도 존재하지 않는다면 지뢰가 없는 인접한 칸들이 자동으로 열림
+				// 보드 출력
+				// 다시 칸 선택
+				Console.WriteLine("Clear");
+			}
+		}
 	}
 }
--- src/Minesweeper/Minesweeper.cs
+++ src/Minesweeper/Minesweeper.cs
@@ -4,6 +4,58 @@ namespace Minesweeper
 {
 	internal class Minesweeper
 	{
+		public static void ShowBoard(int[,] board)
+		{
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					if (board[i, j] != -1)
+					{
+						Console.Write(" 0 ");
+					}
+					else
+					{
+						Console.Write(" 1 ");
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+
+		public static void MakeMine(int[,] board)
+		{
+			int[] mine = new int[10];
+			CreateMine(mine);
+
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					for (int k = 0; k < mine.Length; k++)
+					{
+						if (board[i, j] == mine[k] || board[i, j] == mine[9])
+						{
+							board[i, j] = -1;
+						}
+					}
+				}
+			}
+		}
+
+		private static void CreateMine(int[] mine)
+		{
+			Random rand = new Random();
+			mine[0] = rand.Next(81) + 1;
+			for (int i = 1; i < mine.Length; i++)
+			{
+				mine[i] = rand.Next(81) + 1;
+				if (mine[i] == mine[i - 1])
+				{
+					i--;
+				}
+			}
+		}
 
 	}
 }
