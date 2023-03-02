124d1f8735a771c56e2f2a8324d9510717f7a82b
DragonT13 <alex23456@naver.com>
2023-03-02 23:56:42 +0900

    Minesweeper wip

--- src/Minesweeper/MainClass.cs
+++ src/Minesweeper/MainClass.cs
@@ -6,39 +6,81 @@ namespace Minesweeper
 	{
 		public static void Main(string[] args)
 		{
-			int[,] board = new int[9, 9]; //상수로
+			const int ROW = 9;
+			const int COL = 9;
+			const int MINES_COUNT = 10;
+
+			int[,] board = new int[ROW, COL]; //상수로
 			int[,] mines;
-			bool[,] boardFlag = new bool[9, 9];
+			bool[,] boardFlag = new bool[ROW, COL];
 
 			// 지뢰 생성, 지뢰배열값 출력
-			mines = MakeMines(9, 9, 10);
-			board = MakeMinesweeperBoard(9, 9, mines);
+			mines = MakeMines(ROW, COL, MINES_COUNT);
+			board = MakeMinesweeperBoard(ROW, COL, mines);
 
-			//PrintMinesForTest
+			PrintMinesForTest(mines);
+			Console.WriteLine("----------------------------");
+			PrintMinesweeperBoard(board);
+
+			//// while문으로 play
+			//while ()
+			//{
+			//	ShowBoard(board, board_bools);
+			//	Console.Write("선택할 칸을 입력하시오(1 1 ~ 9 9): ");
+			//	//int[,] inputCoordinate = GetInput()
+			//	string input = Console.ReadLine();
+			//	string[] str = input.Split(" ");
+			//	int[] integers = new int[2];
+			//	for (int i = 0; i < integers.Length; i++)
+			//	{
+			//		integers[i] = int.Parse(str[i]);
+			//	}
+
+			//	boardFlag[inputCoordinate[0], inputCoordinate[1]] = true;
+			//	if (지뢰아니었다면)
+			//	{
+			//		//Traversal => 0인칸들 열리게(재귀이용)
+			//	}
+			//	else
+			//	{
+			//		Console.WriteLine("지뢰를 밟았습니다!\nGame Over!");
+			//		break;
+			//	}
+			//}
+		}
+
+		private static int[,] MakeMines(int row, int col, int minesCount)
+		{
+			Random random = new Random();
+			int[,] mine = new int[minesCount, 2];
+			for (int i = 0; i < minesCount; i++)
+			{
+				mine[i, 0] = random.Next(9);
+				mine[i, 1] = random.Next(9);
+			}
+			return mine;
+		}
+
+		private static int[,] MakeMinesweeperBoard(int row, int col, int[,] mines)
+		{
+			int[,] minesweeperBoard = new int[row, col];
 			for (int i = 0; i < mines.GetLength(0); i++)
 			{
-				Console.Write("{0}, {1}", mines[i, 0], mines[i, 1]);
-				Console.WriteLine();
-			}
-
-			Console.WriteLine("----------------------------");
-
-			for (int k = 0; k < mines.GetLength(0); k++)
-			{
-				int x = mines[k, 0];
-				int y = mines[k, 1];
-				board[x, y] = -1;
+				int x = mines[i, 0];
+				int y = mines[i, 1];
+				minesweeperBoard[x, y] = -1;
 			}
 
 			// 지뢰 주변에 숫자 배정
-			for (int i = 0; i < board.GetLength(0); i++)
+			for (int i = 0; i < minesweeperBoard.GetLength(0); i++)
 			{
-				for (int j = 0; j < board.GetLength(1); j++)
+				for (int j = 0; j < minesweeperBoard.GetLength(1); j++)
 				{
 					for (int k = i - 1; k <= i + 1; k++)
 					{
 						for (int l = j - 1; l <= j + 1; l++)
 						{
+							int mineCount = 0;
 							if (k < 0 || k >= 9 || l < 0 || l >= 9)
 								continue;
 
@@ -48,8 +90,20 @@ namespace Minesweeper
 					}
 				}
 			}
+			return minesweeperBoard;
+		}
 
-			// 보드출력함수를 만들어.
+		private static void PrintMinesForTest(int[,] mines)
+		{
+			for (int i = 0; i < mines.GetLength(0); i++)
+			{
+				Console.Write("{0}, {1}", mines[i, 0], mines[i, 1]);
+				Console.WriteLine();
+			}
+		}
+
+		private static void PrintMinesweeperBoard(int[,] board)
+		{
 			for (int i = 0; i < board.GetLength(0); i++)
 			{
 				for (int j = 0; j < board.GetLength(1); j++)
@@ -57,43 +111,6 @@ namespace Minesweeper
 					Console.Write(" {0} ", board[i, j]);
 				}
 				Console.WriteLine();
-			}
-
-			// while문으로 play
-			while ()
-			{
-				ShowBoard(board, board_bools);
-				Console.Write("선택할 칸을 입력하시오(1 1 ~ 9 9): ");
-				//int[,] inputCoordinate = GetInput()
-				string input = Console.ReadLine();
-				string[] str = input.Split(" ");
-				int[] integers = new int[2];
-				for (int i = 0; i < integers.Length; i++)
-				{
-					integers[i] = int.Parse(str[i]);
-				}
-
-				boardFlag[inputCoordinate[0], inputCoordinate[1]] = true;
-				if (지뢰아니었다면)
-				{
-					//Traversal => 0인칸들 열리게(재귀이용)
-				}
-				else
-				{
-					Console.WriteLine("지뢰를 밟았습니다!\nGame Over!");
-					break;
-				}
-			}
-		}
-
-		private static void MakeMines(int[,] mines)
-		{
-			Random random = new Random();
-
-			for (int i = 0; i < mines.GetLength(0); i++)
-			{
-				mines[i, 0] = random.Next(9);
-				mines[i, 1] = random.Next(9);
 			}
 		}
 
